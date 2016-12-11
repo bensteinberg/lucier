8e316d5f2ddd35ea7c85ed467c22bc0d39b64a8f
Ben Steinberg <benjamin_steinberg@harvard.edu>
2016-12-11 13:56:59 -0500

    Make timestamp consistent for a whole run, make sample rate an argument

--- lucier.py
+++ lucier.py
@@ -4,17 +4,18 @@ import soundfile as sf
 from thready import Playback
 from datetime import datetime
 
-def sitting(input_file, output_base, cycles, playback_device, recording_device):
+def sitting(input_file, output_base, cycles, playback_device, recording_device, fs = 44100):
     """I am sitting in a room"""
+    timestamp = datetime.now().strftime("%Y%m%d%H%M")
     for i in range (0, cycles):
-        output_file = "%s_%s_%d.wav" % (output_base, datetime.now().strftime("%Y%m%d%H%M"), i)
+        output_file = "%s_%s_%d.wav" % (output_base, timestamp, i)
         # try-retry from http://stackoverflow.com/a/2083996
         while True:
             try:
                 t = Playback(input_file, playback_device)
                 t.start()
                 # how to manage levels?  normalize here?  in midside?  need to watch levels manually?
-                midside.record(output_file, recording_device, t.samps, 44100)
+                midside.record(output_file, recording_device, t.samps, fs)
                 input_file = output_file
             except PortAudioError:
                 continue
